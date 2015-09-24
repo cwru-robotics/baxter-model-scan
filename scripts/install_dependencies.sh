126528702f8916d0c87d14bc385eaf3d35e845c8
lucbettaieb <luc.bettaieb@gmail.com>
2015-09-24 14:01:53 -0400

    Added install dependencies.sh

---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo apt-get install ros-indigo-controller-interface ros-indigo-gazebo-ros-control ros-indigo-joint-state-controller ros-indigo-effort-controllers ros-indigo-moveit-msgs
